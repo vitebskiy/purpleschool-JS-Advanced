e37e6efdebb5c1624396346dd3acbb4db777e4e5
Mihail <mihan1993@inbox.ru>
2026-03-11 14:06:11 +0300

    13-buttons

--- main.js
+++ main.js
@@ -1,31 +1,12 @@
 "use strict";
 
-function getCoordinates() {
-  return new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        resolve({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-        });
-      },
-      (error) => {
-        reject(error);
-      }
-    );
-  });
-}
+"use strict";
 
-getCoordinates()
-  .then(({ latitude, longitude }) => {
-    console.log("latitude:", latitude);
-    console.log("longitude:", longitude);
-  })
-  .catch((error) => {
-    if (error.code === 1) {
-      console.log("Геолокация запрещена пользователем (code 1). Разреши доступ в браузере.");
-      return;
-    }
+const wrapper = document.querySelector(".content-wrapper");
+const counter = document.querySelector(".counter__value");
 
-    console.log("Ошибка геолокации:", error);
-  });
+wrapper.addEventListener("click", (event) => {
+  const currentItem = event.target.closest(".btn");
+  currentItem.textContent = `Нажата`;
+  counter.textContent++;
+});
