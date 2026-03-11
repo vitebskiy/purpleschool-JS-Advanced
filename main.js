35963fbe8c0202c1ffa478d26fef880400aa9adf
Mihail <mihan1993@inbox.ru>
2026-03-11 14:09:41 +0300

    fix

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 "use strict";
 
-"use strict";
-
 const wrapper = document.querySelector(".content-wrapper");
 const counter = document.querySelector(".counter__value");
 
 wrapper.addEventListener("click", (event) => {
   const currentItem = event.target.closest(".btn");
+
+  if (!currentItem) return; 
+
   currentItem.textContent = `Нажата`;
   counter.textContent++;
 });
