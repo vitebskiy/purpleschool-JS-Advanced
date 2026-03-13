5728a40e7bfa52d67ce5aec7d1406aa767a0b7c7
Mikhail Vitebskiy <mihan1993@inbox.ru>
2026-03-13 14:01:38 +0300

    14-module

---
+++ task.js
@@ -0,0 +1,15 @@
+// export class Task {
+//   constructor(message) {
+//     this.message = message;
+//   }
+
+//   run() {
+//     console.log(this.message);
+//   }
+// }
+
+export class Task {
+  run() {
+    console.log("писать код ");
+  }
+}
